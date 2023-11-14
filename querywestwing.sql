bf7062a6e26c48dc6e104fedc1034ec70eafe656
André Troise Agostinis <andre.t.agostinis@gmail.com>
2023-11-13 23:13:31 -0300

    Add files via upload

---
+++ querywestwing.sql
@@ -0,0 +1,16 @@
+USE westwingdb;
+
+SELECT cliente.id_cliente      AS ID_Cliente,
+       cliente.nome            AS Nome,
+       cliente.sobrenome       AS Sobrenome,
+       cliente.data_nascimento AS Data_de_nascimento,
+       casa.cor                AS Cor_da_Casa,
+       bairro.nome             AS Bairro,
+       carro.modelo            AS Carro
+FROM   cliente
+       LEFT JOIN casa
+              ON cliente.id_cliente = casa.fk_cliente
+       LEFT JOIN bairro
+              ON casa.fk_bairro = bairro.id_bairro
+       LEFT JOIN carro
+              ON cliente.id_cliente = carro.fk_cliente;
